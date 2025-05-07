bba61dfa02c85bab3694274176229116818ab426
pdsong <songpeidong@gmail.com>
2025-05-08 01:12:16 +0800

    添加产品的创建 更新 删除

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,43 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show update destroy]
+  before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update destroy]
+
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head 204
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def check_owner
+    head :forbidden unless current_user && current_user.id == @product.user_id
+  end
   def index
     render json: Product.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: %i[create]
-      resources :products, only: %i[index show]
+      resources :products, only: %i[index show create update destroy]
     end
   end
 end
